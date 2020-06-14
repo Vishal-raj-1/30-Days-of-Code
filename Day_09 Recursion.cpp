301d79ccd71f7cb91c66373161476740a5afd451
Vishal-raj-1 <rajputvishal33786@gmail.com>
2020-06-14 16:28:21 +0530

    Day_09 Recursion.cpp added

---
+++ Day_09 Recursion.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int n)
+{
+    if(n <= 1)
+        return 1;
+
+    return n*factorial(n-1);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int result = factorial(n);
+
+    cout << result << "\n";
+
+    return 0;
+}
